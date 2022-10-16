e05b828e5d417cc02b21912c4b53bf629ffe56c7
Welde-Ab <35730399+Welde-Ab@users.noreply.github.com>
2022-10-16 22:34:29 +0900

    change mode

--- models/state.py
+++ models/state.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from sqlalchemy.sql.schema import ForeignKey
 from models.base_model import BaseModel, Base
+from models import storage_type
 from models.city import City
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class / table model"""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
         @property
         def cities(self):
-            """"""""
-            list_c = []
-            for city in models.storage.all(City).values():
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
                 if city.state_id == self.id:
-                    list_c.append(city)
-            return list_c
+                    related_cities.append(city)
+            return related_cities
